c631adf4b9a722fcd9613d13b53d97fdfb0c7ee1
MariaSantanaM <maria.lsm.0306@gmail.com>
2024-03-07 14:58:34 -0300

    arreglo ruta y estructura home

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,14 +1,21 @@
-
+import { Stack, Chip, Container } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 function Home() {
+  return (
 
-    return (
-        <>
-          <h1>Home</h1>
-
-        </>
-        
-    );
-    
-}
+    <>
+      <div className="title-container">
+        <h1 className='titulo'>Cambio de Turnos</h1>
+      </div>
+      <Container className='container'>
+        <Stack direction="column" spacing={2}>
+          <Chip label="Turno Actual" component={Link} to="#basic-chip" variant="outlined" clickable className="chip" />
+          <Chip label="Turno Anterior" component={Link} to="#basic-chip" variant="outlined" clickable className="chip" />
+          <Chip label="Subir Archivo" component={Link} to="/UploadDocument" variant="outlined" clickable className="chip" />
+        </Stack>
+      </Container>
+    </>
+  );
+};
 export default Home;
