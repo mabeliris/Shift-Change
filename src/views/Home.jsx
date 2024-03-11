f8a066e210bb0811fc0f0e9160273414ef95ed0a
MayAlcione <myalcione@gmail.com>
2024-03-11 01:29:07 -0300

    Adicción de fecha y hora, correción de diseño.

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,4 +1,4 @@
-
+import { useState, useEffect } from 'react'; // Importar los hooks useState y useEffect
 import { Stack, Chip, Container } from '@mui/material';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
@@ -14,7 +14,6 @@ const StyledContainer = styled(Container)`
   background-color: #252525;
   border-radius: 10px;
   color: #e46545; /* Cambia el color de la fuente aquí */
-  float: left; /* Mueve el contenedor hacia la izquierda */
 `;
 
 const StyledChip = styled(Chip)`
@@ -32,7 +31,7 @@ const ImageContainer = styled.div`
 `;
 
 const StyledImage = styled.img`
-  width: 200px; /* Define el tamaño de la imagen */
+  width: 400px; /* Define el tamaño de la imagen */
   height: auto; /* Altura automática para mantener la proporción */
   display: center; /* Para centrar la imagen horizontalmente */
   margin:  40px; /* Centra la imagen horizontalmente */
@@ -40,27 +39,54 @@ const StyledImage = styled.img`
 `;
 
 function Home() {
+  const [currentDate, setCurrentDate] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurrentDate(new Date());
+    }, 1000); // Actualizar la fecha cada segundo
+
+    return () => clearInterval(intervalId); // Limpiar el intervalo cuando el componente se desmonta
+  }, []); // Dependencia vacía para que el useEffect solo se ejecute una vez
+
   return (
     <>
       <TitleContainer>
-        <h1 className='titulo'>Cambio de Turnos</h1>
+      <h1 className='titulo' style={{ textAlign: 'center',marginTop:'20px', marginLeft: '350px', marginBottom: '70px', color: '#c2c2c2' }}>Cambio de Turnos</h1>    
       </TitleContainer>
-      <ImageContainer>
-        {/* Aquí puedes poner tu imagen */}
-        <StyledImage src="src/assets/Bhp_logo_PNG1-1.png" alt="Descripción de la imagen" />
-      </ImageContainer>
-      <StyledContainer>
-        <Stack direction="column" spacing={2}>
-          <StyledChip label="Turno Actual" component={Link} to="#basic-chip" variant="outlined" clickable className="chip" />
-          <StyledChip label="Turno Anterior" component={Link} to="#basic-chip" variant="outlined" clickable className="chip" />
-          <StyledChip label="Subir Archivo" component={Link} to="/UploadDocument" variant="outlined" clickable className="chip" />
-        </Stack>
-      </StyledContainer>
-      <Container className='container-end' style={{ position: 'fixed', bottom: 0, left: 1200, textAlign: 'rigth', padding: '10px' }}>
-        <Stack direction="" spacing={10}>
-          <Chip label="Salir" component={Link} to="#basic-chip" variant="outlined" clickable className="chip" />
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+  <StyledContainer>
+    <Stack direction="column" spacing={2}>
+      <StyledChip label="Turno Actual" component={Link} to="#basic-chip" variant="outlined" clickable className="chip" style={{
+        fontSize: '24px', color: '#d9d9d9', borderColor: '#E35302',
+        borderWidth: '2px', width: '250px', padding: '30px 0px'
+      }} />
+      <StyledChip label="Turno Anterior" component={Link} to="#basic-chip" variant="outlined" clickable className="chip" style={{
+        fontSize: '24px', color: '#d9d9d9', borderColor: '#E35302',
+        borderWidth: '2px', width: '250px', padding: '30px 0px'
+      }} />
+      <StyledChip label="Subir Archivo" component={Link} to="/UploadDocument" variant="outlined" clickable className="chip" style={{
+        fontSize: '24px', color: '#d9d9d9', borderColor: '#E35302',
+        borderWidth: '2px', width: '250px', padding: '30px 0px'
+      }} />
+    </Stack>
+  </StyledContainer>
+  <ImageContainer style={{ marginLeft: '20px' }}>
+    {/* Aquí puedes poner tu imagen */}
+    <StyledImage src="src/assets/Bhp_logo_PNG1-1.png" alt="Descripción de la imagen" style={{ direction: 'column', marginBottom: '50px' }} />
+  </ImageContainer>
+</div>
+
+      <Container className='container-end' style={{ position: 'fixed', bottom: 30, left: 1000, textAlign: 'rigth', padding: '10px' }}>
+        <Stack direction="" spacing={0}>
+          <Chip label="Salir" component={Link} to="#basic-chip" variant="outlined" clickable className="chip" style={{
+            fontSize: '24px', color: '#d9d9d9', borderColor: '#E35302',
+            borderWidth: '2px', width: '250px',padding: '30px 0px'
+          }} />
+          
         </Stack>
       </Container>
+      <p style={{ fontSize: '24px', color: '#c2c2c2',  position: 'fixed', top:'570px', bottom: 10, left: 950, textAlign: 'rigth' }}>Fecha: {currentDate.toLocaleDateString()} Hora: {currentDate.toLocaleTimeString()}</p>
     </>
   );
 };
